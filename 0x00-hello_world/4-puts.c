76279891f82cd957ddffd52f53a70f8e84b634e1
Lysa1nd <lysandrawakonyo4@gmail.com>
2023-04-20 15:19:04 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main  -print a given task
+ *
+ * Return: Always 0 success, non zero otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
