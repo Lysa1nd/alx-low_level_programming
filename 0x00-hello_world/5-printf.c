d7c6c18556908d6506214ff53ea359e0e73e356d
Lysa1nd <lysandrawakonyo4@gmail.com>
2023-04-20 17:42:29 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-
 /**
- * main-it outputs the phrase with proper grammar,
- * but the outcome is the piece of art,
- * Return:Always 0 is exicted,otherwise return a non zero int
+ *main - it outputs the phrase with proper grammar,
+ *but the outcome is the piece of art,
+ *Return: Always 0 is exited, otherwise return a non zero int
  */
-intmain(void)
+
+int main(void)
 {
-	printf("with proper grammar,but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
