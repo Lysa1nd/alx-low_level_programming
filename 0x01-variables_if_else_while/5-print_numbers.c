5a681553c4ce64db644a2067595f33e4fc1cb093
Lysa1nd <lysandrawakonyo4@gmail.com>
2023-04-24 18:02:42 -0700

    task 1 last digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print numbers of base 10'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a =0; a <= 9; a++)
+	{
+		printf("%d", d);
+	}
+	printf("\n");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,7 @@
+#include<stdio.h>                        /**                                       * main - Entry point                     * Description: 'print numbers with base  * 10 using putchar'
+ * Return: Always 0                       */                                      int main(void)                           {
+        int d;
+                                                 for(d = 0; d <= 9; d++)                  {
+                putchar(n + 48);
+        }
+        putchar('\n');                           return (0);                      }
