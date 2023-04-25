ee7efc610e8b4e531498850fb1cc15bfaa25d8b4
Lysa1nd <lysandrawakonyo4@gmail.com>
2023-04-25 00:49:31 -0700

    task 1 last digit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+/**
+ * main - shows all different combined three digit numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int t, u, v;
+
+	for ( t = 48, t < 58, t++)
+	{
+		for (u = 49, u < 58, u++) 
+		{
+			for ( v = 50, v < 58, v++)
+		{
+			if ( v > u && u >t)
+			{
+				putchar(t);
+				putchar(u);
+				putchar(v);
+				if (t != 55 || u != 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
