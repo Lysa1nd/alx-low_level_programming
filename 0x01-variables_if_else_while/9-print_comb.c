f4834b6e2d5a258931117dd2b3b683e1eac74fa7
Lysa1nd <lysandrawakonyo4@gmail.com>
2023-04-24 19:17:23 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'print all combined single digit numbers'
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 48; m < 58; m++)
+	{
+		putchar(m);
+		if (m != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
