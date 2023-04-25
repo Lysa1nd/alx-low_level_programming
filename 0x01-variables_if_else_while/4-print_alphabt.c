1e5e0d27a0b314a9c3402c569977e864175cd37b
Lysa1nd <lysandrawakonyo4@gmail.com>
2023-04-24 17:13:03 -0700

    task 4 without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print the alphabets apart from q and e'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int b = 97;
+
+	while  (b <= 122)
+	{
+		if (b == 101 || b == 113)
+		{
+			b++;
+			continue;
+		}
+		putchar(b);
+		b++;
+	}
+	putchar('\b');
+	return (0);
+}
+
