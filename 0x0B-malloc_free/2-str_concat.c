dbfca8c8526662a723b890d2cea2af5a2040938f
Lysa1nd <lysandrawakonyo4@gmail.com>
2023-05-16 13:53:48 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,9 +3,9 @@
 
 /**
  * str_concat - get ends of input and add together for size
- * @1: input one to concat
- * @2: input two to concat
- * Return concat of s1 and s2
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
  */
 char *str_concat(char *s1, char *s2)
 {
