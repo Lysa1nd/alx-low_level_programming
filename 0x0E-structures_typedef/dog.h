b3a34adf28da03d8b63d6671bbb1a21ab8bf83e6
Lysa1nd <lysandrawakonyo4@gmail.com>
2023-05-22 16:40:00 -0700

    task 0 and 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+
 /**
- * struct_dog - a dog's basic info
+ * struct dog - a dog's basic info
  * @name: First member
  * @age: Second member
- * @owner: third member
+ * @owner: Third member
+ *
  * Description: Longer description
  */
 struct dog
@@ -13,7 +15,8 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
 
 /**
  * dog_t - typedef for struct dog
@@ -26,5 +29,6 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
+
 #endif
 
