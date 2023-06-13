fbb714d81c3a759a42ad72a5e5219e0f036375e4
Lysa1nd <lysandrawakonyo4@gmail.com>
2023-06-13 14:21:00 -0700

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * read_textfile -  Reads text file and 
+ * prints it to the POSIX standard output
+ * @filename: Points to the filename
+ * @letters: number of letters it reads
+ * and print.
+ * Return: number of letters it reads
+ * and prints return 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int fd;
+    int write_ret;
+    ssize_t read_ret;
+    char *buffer;
+
+    if (filename == NULL)
+        return 0;
+
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return 0;
+
+    buffer = (char *)malloc(sizeof(char) * letters);
+    if (buffer == NULL)
+        return 0;
+
+    read_ret = read(fd, buffer, letters);
+    if (read_ret == -1)
+        return 0;
+
+    close(fd);
+
+    write_ret = write (STDOUT_FILENO, buffer, read_ret);
+    free(buffer);
+    if (write_ret != read_ret)
+        return 0;
+
+    return read_ret;
+}
